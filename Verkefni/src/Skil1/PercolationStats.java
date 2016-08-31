83a2832dcea9b3cbf7f3702d71e5ceefd6e530ba
baldurp12 <baldurp12@ru.is>
2016-08-31 14:18:02 +0000

    Init

---
+++ Verkefni/src/Skil1/PercolationStats.java
@@ -0,0 +1,54 @@
+package Skil1;
+
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
+import edu.princeton.cs.algs4.QuickFindUF;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class PercolationStats {
+	
+	private static double[] openedSites; 
+	
+	public PercolationStats(int N, int T){ // perform T independent experiments on an N-by-N grid
+		
+		
+		if(N <= 0 || T <= 0){
+				throw new IllegalArgumentException();
+		}
+		
+		openedSites = new double[T];
+		
+		for(int i = 0; i < T; i++){
+			Percolation block = new Percolation(N);
+			while(!block.percolates()){
+				block.open(StdRandom.uniform(N), StdRandom.uniform(N));
+			}
+			openedSites[i] = block.numberOfOpenSites();
+		}
+		
+	}
+	 
+	public double mean(){ // sample mean of percolation threshold
+		return StdStats.mean(openedSites);
+	} 
+	 
+	 
+	public double stddev(){ // sample standard deviation of percolation threshold
+		return StdStats.stddevp(openedSites);
+	} 
+	 
+	 
+	public double confidenceLow(){ // low end point of 95%confidence interval
+		/*
+		 * (1.96)*mean() minus 
+		 * java.lang.Math.sqrt(openedSites.length)
+		 */
+		return 0.0;
+	} 
+	 
+	 
+	public double confidenceHigh() { // high end point of 95% confidence interval
+		return 0.0; //TODO 	 
+	}
+	
+}
---
+++ src/Skil_1/Percolation.java
@@ -0,0 +1,86 @@
+package Skil_1;
+
+import edu.princeton.cs.algs4.*;
+
+
+/**
+ * @author Baldur Mar
+ * @author Thorgerdur Edda
+ * @instructor Magnus Mar Halldorsson
+ * <p>
+ * Hand-in assignment 1 for T-301-REIR Autumn semester 2016
+ * @category School assignment for T-301-REIR, Reykjavik University
+ */
+
+
+public class Percolation {
+
+    private static int[][] grid; // grid[rows][columns]
+    private static int gridSize;
+    protected static int openSites;
+
+
+    public static void main(String[] args) {// unit testing (required)
+        PercolationStats tester = new PercolationStats(5, 5);
+        StdOut.print("IT RAN !!");
+
+    }
+
+    // These functions were provided by the instructor, implementation is a part of the assignment
+
+    /**
+     * Class constructor
+     * Sets grid size to N
+     * and sets all sites to blocked
+     *
+     * @param N
+     */
+    public Percolation(int N) {// create N-by-N grid, with all sites initially blocked, blocked == 0
+        grid = new int[N][N];
+        gridSize = N;
+        openSites = 0;
+
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                grid[i][j] = 0;
+            }
+        }
+    }
+
+
+    public void open(int row, int col) { // open the site (row, col) if it is not open already
+        if (grid[row][col] == 0) grid[row][col] = 1;
+        openSites++;
+    }
+
+    public boolean isOpen(int row, int col) { // is the site (row, col) open?
+        return grid[row][col] == 1;
+    }
+
+    public boolean isFull(int row, int col) { // is the site (row, col) full?
+        return true; // TODO - Perform UnionFind magic
+    }
+
+    /**
+     * Returns the number of sites open in the grid
+     *
+     * @return int openSites
+     */
+    public int numberOfOpenSites() {
+
+        return openSites;
+    }
+
+    /**
+     * Returns true for now
+     *
+     * @return
+     */
+    public boolean percolates() { // does the system percolate?
+        return true; // TODO - Perform UnionFind magic
+    }
+
+
+}
+
+
