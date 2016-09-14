4ed78be908d4810d14771869a30c374ff897fe18
Thorgerdur Eiriksdottir <thorgerdur14@ru.is>
2016-09-14 18:54:59 +0000

    laga sma brute

--- src/Skil_2/Brute.java
+++ src/Skil_2/Brute.java
@@ -11,23 +11,24 @@ public class Brute {
 
     public static boolean checkifEqual(Point p, Point q, Point r, Point s) {
 
-        return p.slopeTo(q) == p.slopeTo(r) || p.slopeTo(r) == p.slopeTo(s);
+        if(p.slopeTo(q) == p.slopeTo(r)){
+            if (p.slopeTo(q) == p.slopeTo(s)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void collinear (Point p[]) {
         int c = p.length;
         Arrays.sort(p);
-        
+        // 4 x for loop because O(n^4)
         for (int i = 0; i < c; i++) {
             for (int j = i + 1; j < c; j++) {
-                for (int k = j; k < c; k++) {
-                    for (int l = k; l < c; l++) {
+                for (int k = j + 1; k < c; k++) {
+                    for (int l = k + 1; l < c; l++) {
                         if (checkifEqual(p[i], p[j], p[k], p[l])) {
-
-                            System.out.println(p[i].toString() + " -> " +
-                                p[j].toString() + " -> " +
-                                p[k].toString() + " -> " +
-                                p[l].toString());
+                            System.out.println(p[i].toString() + " -> " + p[j].toString() + " -> " + p[k].toString() + " -> " + p[l].toString());
                         }
                     }
                 }
@@ -35,12 +36,8 @@ public class Brute {
         }
     }
 
-
-
     public static void main(String[] args) {
-        /*
-         * Do not modify
-         */
+
         In in = new In();
         int n = in.readInt();
         Point[] points = new Point[n];
