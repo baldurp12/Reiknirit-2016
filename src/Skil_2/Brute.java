2007c40686cd61f116b285aac136a76fa260d0fc
Thorgerdur Eiriksdottir <thorgerdur14@ru.is>
2016-09-15 15:25:45 +0000

    done

--- src/Skil_2/Brute.java
+++ src/Skil_2/Brute.java
@@ -1,6 +1,9 @@
 package Skil_2;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
 import java.util.Arrays;
 
 /**
@@ -20,15 +23,21 @@ public class Brute {
     }
 
     public static void collinear (Point p[]) {
-        int c = p.length;
         Arrays.sort(p);
+        int len = p.length;
+
         // 4 x for loop because O(n^4)
-        for (int i = 0; i < c; i++) {
-            for (int j = i + 1; j < c; j++) {
-                for (int k = j + 1; k < c; k++) {
-                    for (int l = k + 1; l < c; l++) {
+        for (int i = 0; i < len; i++) {
+
+            for (int j = i + 1; j < len; j++) {
+
+                for (int k = j + 1; k < len; k++) {
+
+                    for (int l = k + 1; l < len ; l++) {
+
                         if (checkifEqual(p[i], p[j], p[k], p[l])) {
-                            System.out.println(p[i].toString() + " -> " + p[j].toString() + " -> " + p[k].toString() + " -> " + p[l].toString());
+
+                            StdOut.println(p[i].toString() + " -> " + p[j].toString() + " -> " + p[k].toString() + " -> " + p[l].toString());
                         }
                     }
                 }
@@ -38,7 +47,9 @@ public class Brute {
 
     public static void main(String[] args) {
 
-        In in = new In();
+        Stopwatch timer = new Stopwatch();
+
+        In in = new In("/Users/thorgerdureiriksdottir/Desktop/REIR/Reiknirit-2016/lib/testinput/input6.txt");
         int n = in.readInt();
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
@@ -47,6 +58,11 @@ public class Brute {
 
         }
         collinear(points);
+
+        double t = timer.elapsedTime();
+        StdOut.print("time:" + t);
+
+
     }
 }
 
