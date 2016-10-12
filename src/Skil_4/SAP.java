91888c2b16aa6f3e253b13f94bde8dd5349a882f
Thorgerdur Eiriksdottir <thorgerdur14@ru.is>
2016-10-12 16:00:14 +0000

    SAP acyclic

--- src/Skil_4/SAP.java
+++ src/Skil_4/SAP.java
@@ -1,16 +1,26 @@
 package Skil_4;
 
-import edu.princeton.cs.algs4.Bag;
-import edu.princeton.cs.algs4.Digraph;
-import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
+import edu.princeton.cs.algs4.*;
 
 
 public class SAP {
 
+    private static Digraph digraph;
+
     // constructor takes a digraph ( not necessarily a DAG )
     public SAP(Digraph G){
+        this.digraph = new Digraph(G);
+        StdOut.println("haehae");
+        DirectedCycle cycle = new DirectedCycle(digraph);
+        if(cycle.hasCycle()){
+            throw new IllegalArgumentException("Graph is not acyclic");
+        }
+        else{
+            StdOut.println("is acyclic");
+        }
 
     }
+
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) throws IndexOutOfBoundsException{
@@ -34,9 +44,11 @@ public class SAP {
 
     // do unit testing of this class
     public static void main(String[] args){
+        In in = new In("/Users/thorgerdureiriksdottir/Desktop/Reikni/Reiknirit-2016/lib/Skil_4/wordnet/digraphs/digraph1.txt");
+        Digraph digraph = new Digraph(in);
+        SAP testSap = new SAP(digraph);
 
         Bag<Integer> hypernyms = new Bag<Integer>();
-
 
 
     }
