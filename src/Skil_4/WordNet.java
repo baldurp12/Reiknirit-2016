c8c4e50fa70eff7b5d75480a09c03166a71effb4
Baldur Mar <baldurp12@ru.is>
2016-10-13 21:35:22 +0000

    Working on WordNet

--- src/Skil_4/WordNet.java
+++ src/Skil_4/WordNet.java
@@ -7,7 +7,7 @@ import edu.princeton.cs.algs4.*;
 
 public class WordNet {
 
-    private  SAP s;
+    private  SAP sap;
     private  Digraph digraph;
     private  ST<String, Bag<Integer>> nouns;
     private  ST<Integer, String> synsets;
@@ -15,42 +15,82 @@ public class WordNet {
     // constructor takes the name of the two input files
     public WordNet ( String synsets , String hypernyms ) throws IllegalArgumentException{
         int counter = 0;
-        //Read synets from file.
+        //Read synsets from file.
         In in = new In(synsets);
 
         //initialize
         this.nouns = new ST<String, Bag<Integer>>();
         this.synsets = new ST<Integer, String>();
 
+
+        /**
+         * Build the two STs, synsets and nouns
+         */
         while(in.hasNextLine()){
             String[] splitline = in.readLine().split(",");
-            for (String s : splitline[1].split(" ")){
-                Bag<Integer>
+            int synsetsID = Integer.parseInt(splitline[0]);
+            String[] nounArray = splitline[1].split(" ");
+
+            this.synsets.put(synsetsID, splitline[1]);
+
+            for(String s: nounArray){
+                if(this.nouns.contains(s)){
+                    this.nouns.get(s).add(synsetsID);
+                }
+                else{
+                    Bag<Integer> newBag = new Bag<Integer>();
+                    newBag.add(synsetsID);
+                    this.nouns.put(s, newBag);
+                }
             }
+
 
         }
 
+        // Close the In stream used to read the synsets
+        in.close();
 
-        this.digraph = new Digraph(counter);
+        // Open a new In stream with the hypernym file
+        in = new In(hypernyms);
 
 
-        s = new SAP(digraph);
+
+        /**
+         * Builds the hypernym digraph with the same number of Vertices as there are keys in the {@param}synsets ST
+         */
+
+        digraph = new Digraph(this.synsets.size());
+
+        while(in.hasNextLine()){
+            String[] nodes = in.readLine().split(",");
+            for(String s: nodes){
+                if(!(s.equals(nodes[0]))){
+                    digraph.addEdge(Integer.parseInt(nodes[0]), Integer.parseInt(s));
+                }
+            }
+        }
+        // Close the In stream used to read hypernyms
+        in.close();
+
     }
 
     // returns all WordNet nouns
     public Iterable <String > nouns (){
-        return null;
+
+        return this.nouns.keys();
     }
 
     // is the word a WordNet noun ?
     public boolean isNoun ( String word ){
-        return false;
+
+        return this.nouns.contains(word);
     }
 
      // distance between nounA and nounB ( defined below )
      public int distance ( String nounA , String nounB ){
 
-         return -1;
+
+
      }
 
      // a synset ( second field of synsets .txt ) that is a shortest common ancestor
@@ -61,6 +101,12 @@ public class WordNet {
 
      // do unit testing of this class
      public static void main ( String [] args ){
+         String synsetsFile = "C:\\Users\\Baldur\\Dropbox\\Bs.C-Hugb\\5.onn\\reir\\Reiknirit-2016\\lib\\Skil_4\\wordnet\\synsets\\synsets100-subgraph.txt";
+         String hypernymsFile = "C:\\Users\\Baldur\\Dropbox\\Bs.C-Hugb\\5.onn\\reir\\Reiknirit-2016\\lib\\Skil_4\\wordnet\\hypernyms\\hypernyms100-subgraph.txt";
+
+         WordNet newNet = new WordNet(synsetsFile, hypernymsFile);
+
+         StdOut.println(newNet.digraph);
 
 
      }
