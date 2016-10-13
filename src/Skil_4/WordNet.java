fac4c98ccd8d028de85467a6b20f2a99a39487c5
Baldur Mar <baldurp12@ru.is>
2016-10-13 22:40:41 +0000

    WordNet fail á Mooshak

--- src/Skil_4/WordNet.java
+++ src/Skil_4/WordNet.java
@@ -14,14 +14,12 @@ public class WordNet {
 
     // constructor takes the name of the two input files
     public WordNet ( String synsets , String hypernyms ) throws IllegalArgumentException{
-        int counter = 0;
         //Read synsets from file.
         In in = new In(synsets);
 
         //initialize
         this.nouns = new ST<String, Bag<Integer>>();
         this.synsets = new ST<Integer, String>();
-
 
         /**
          * Build the two STs, synsets and nouns
@@ -54,9 +52,8 @@ public class WordNet {
         in = new In(hypernyms);
 
 
-
         /**
-         * Builds the hypernym digraph with the same number of Vertices as there are keys in the {@param}synsets ST
+         * Builds the hypernym digraph with the same number of Vertices as there are keys in the {@param}synsets
          */
 
         digraph = new Digraph(this.synsets.size());
@@ -69,45 +66,102 @@ public class WordNet {
                 }
             }
         }
+
         // Close the In stream used to read hypernyms
         in.close();
+
+        // Finally create the SAP object containing the digraph
+        sap = new SAP(digraph);
 
     }
 
     // returns all WordNet nouns
     public Iterable <String > nouns (){
 
-        return this.nouns.keys();
+        return nouns.keys();
     }
 
     // is the word a WordNet noun ?
     public boolean isNoun ( String word ){
 
-        return this.nouns.contains(word);
+        return nouns.contains(word);
     }
 
      // distance between nounA and nounB ( defined below )
      public int distance ( String nounA , String nounB ){
 
-
-
+        return sap.length(nouns.get(nounA), nouns.get(nounB));
      }
 
      // a synset ( second field of synsets .txt ) that is a shortest common ancestor
      // of nounA and nounB
      public String sap ( String nounA , String nounB ){
-         return null;
+
+         Bag<Integer> aIds = nouns.get(nounA);
+         Bag<Integer> bIds = nouns.get(nounB);
+
+         int synsetId = sap.ancestor(aIds, bIds);
+         return synsets.get(synsetId);
      }
 
      // do unit testing of this class
      public static void main ( String [] args ){
-         String synsetsFile = "C:\\Users\\Baldur\\Dropbox\\Bs.C-Hugb\\5.onn\\reir\\Reiknirit-2016\\lib\\Skil_4\\wordnet\\synsets\\synsets100-subgraph.txt";
-         String hypernymsFile = "C:\\Users\\Baldur\\Dropbox\\Bs.C-Hugb\\5.onn\\reir\\Reiknirit-2016\\lib\\Skil_4\\wordnet\\hypernyms\\hypernyms100-subgraph.txt";
+         String synsetsFile = "C:\\Users\\Baldur\\Dropbox\\Bs.C-Hugb\\5.onn\\reir\\Reiknirit-2016\\lib\\Skil_4\\wordnet\\synsets\\synsets6.txt";
+         String hypernymsFile = "C:\\Users\\Baldur\\Dropbox\\Bs.C-Hugb\\5.onn\\reir\\Reiknirit-2016\\lib\\Skil_4\\wordnet\\hypernyms\\hypernyms6TwoAncestors.txt";
 
          WordNet newNet = new WordNet(synsetsFile, hypernymsFile);
 
+         /**
+          * List all vertices in the Digraph
+          */
          StdOut.println(newNet.digraph);
 
+         /**
+          * Check if {@method contains} works
+          */
+         StdOut.println("Contains 'Baldur'" + newNet.isNoun("Baldur"));
+         StdOut.println("Contains 'beta_globulin'" + newNet.isNoun("beta_globulin"));
+
+         /**
+          * sap tests
+          */
+
+         StdOut.println(newNet.sap("a", "a"));
+         StdOut.println(newNet.sap("a", "b"));
+         StdOut.println(newNet.sap("a", "c"));
+         StdOut.println(newNet.sap("a", "d"));
+         StdOut.println(newNet.sap("a", "e"));
+         StdOut.println(newNet.sap("a", "f"));
+         StdOut.println(newNet.sap("b", "a"));
+         StdOut.println(newNet.sap("b", "b"));
+         StdOut.println(newNet.sap("b", "c"));
+         StdOut.println(newNet.sap("b", "d"));
+         StdOut.println(newNet.sap("b", "e"));
+         StdOut.println(newNet.sap("b", "f"));
+         StdOut.println(newNet.sap("c", "a"));
+         StdOut.println(newNet.sap("c", "b"));
+         StdOut.println(newNet.sap("c", "c"));
+         StdOut.println(newNet.sap("c", "d"));
+         StdOut.println(newNet.sap("c", "e"));
+         StdOut.println(newNet.sap("c", "f"));
+         StdOut.println(newNet.sap("d", "a"));
+         StdOut.println(newNet.sap("d", "b"));
+         StdOut.println(newNet.sap("d", "c"));
+         StdOut.println(newNet.sap("d", "d"));
+         StdOut.println(newNet.sap("d", "e"));
+         StdOut.println(newNet.sap("d", "f"));
+         StdOut.println(newNet.sap("e", "a"));
+         StdOut.println(newNet.sap("e", "b"));
+         StdOut.println(newNet.sap("e", "c"));
+         StdOut.println(newNet.sap("e", "d"));
+         StdOut.println(newNet.sap("e", "e"));
+         StdOut.println(newNet.sap("e", "f"));
+         StdOut.println(newNet.sap("f", "a"));
+         StdOut.println(newNet.sap("f", "b"));
+         StdOut.println(newNet.sap("f", "c"));
+         StdOut.println(newNet.sap("f", "d"));
+         StdOut.println(newNet.sap("f", "e"));
+         StdOut.println(newNet.sap("f", "f"));
 
      }
 }
